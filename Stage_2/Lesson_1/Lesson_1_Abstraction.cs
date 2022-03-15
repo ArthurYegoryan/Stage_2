29f3f011f211a10a20407144285c6958a749b580
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2022-03-15 14:23:50 +0400

    Added Lesson_1_Abstraction.cs file in Lesson_1 folder

---
+++ Stage_2/Lesson_1/Lesson_1_Abstraction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage_2.Lesson_1
+{
+    abstract class Cars
+    {
+        public abstract void CarModel();             // Abstract method-ը կարող է լինել միայն Abstract class-ում և չի կարող ունենալ body
+
+        public void CarBody()                        // Abstract method-ը կարող է ունենալ նաև սովորական method
+        {
+            Console.WriteLine("Sedan");
+        }
+    }
+
+    class Mercedes : Cars
+    {
+        public override void CarModel()              // Եթե Abstract class-ի հետ ունենք նույն անունով method, ապա պետք է գրենք override, հակառակ դեպքում կստանանք error
+        {
+            Console.WriteLine("Mercedes");
+        }
+    }
+
+    internal class Lesson_1_Abstraction
+    {
+        static void Main()
+        {
+            //Cars cars = new Cars();                 // Abstract class-ի համար չենք կարող ստեղծել instance
+
+            Mercedes mercedes = new Mercedes();
+
+            mercedes.CarModel();
+            mercedes.CarBody();
+        }
+    }
+}
