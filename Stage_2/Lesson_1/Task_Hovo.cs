95709997eef3b448692781b3db62651e7278c6a5
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2022-03-16 17:34:16 +0400

    Added Task_Hovo.cs file in Lesson_1 folder

---
+++ Stage_2/Lesson_1/Task_Hovo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage_2.Lesson_1
+{
+    class Car    // Base
+    {        
+        private string model;
+        public string ModelProperty
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public void PrintModel(string model)
+        {
+            Console.WriteLine($"My car is {model}!");
+        }
+
+        public string color;
+    }
+
+    class BMW : Car
+    {
+        protected string fuelType;
+
+        public void PrintColor(string color)
+        {
+            Console.WriteLine($"My cars color is {color}");
+        }
+    }
+
+    class Series4 : BMW
+    {        
+        public string FuelTypeProperty
+        {
+            get { return fuelType; }
+            set { fuelType = value; }
+        }
+
+        public void PrintFuelType(string fuelType)
+        {
+            Console.WriteLine($"Fuel type is {fuelType}");
+        }
+    }
+
+    class SeriesX6 : BMW
+    {
+        public string FuelTypeProperty
+        {
+            get { return fuelType; }
+            set { fuelType = value; }
+        }
+
+        public void PrintFuelType(string fuelType)
+        {
+            Console.WriteLine($"Fuel type is {fuelType}");
+        }
+    }
+
+    internal class Task_Hovo
+    {
+        static void Main()
+        {
+            //Car car = new Car();
+            BMW bmw = new BMW();
+
+            bmw.ModelProperty = "BMW";
+            bmw.PrintModel(bmw.ModelProperty);
+            bmw.color = "Black";
+            bmw.PrintColor(bmw.color);
+
+            Series4 series4 = new Series4();
+            series4.FuelTypeProperty = "Petrol";
+            series4.PrintFuelType(series4.FuelTypeProperty);
+
+            SeriesX6 seriesX6 = new SeriesX6();
+            seriesX6.FuelTypeProperty = "Diesel";
+            seriesX6.PrintFuelType(seriesX6.FuelTypeProperty);
+        }
+    }
+}
